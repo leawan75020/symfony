c4a7714a5964774a6d90b167b26880526454d4a2
lea WAN <leawan75020@gmail.com>
2023-03-15 09:40:01 +0100

    controller pizza by id

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -28,7 +28,44 @@ class PizzaController extends AbstractController
 
         //enregistrer la pizza dans la bd
         $repository->save($pizza, true);
+        
+        //recuperer une pizza selon id
+        $pizza1 = $repository->find(1);
+        var_dump($pizza1);
+        echo '<br>';
+
+        //recuperer toutes les pizza dans une liste
+        $pizzas = $repository->findAll();
+        var_dump($pizzas);
+        echo '<br>';
+
+        //supprimer une pizza
+        $repository->remove($pizza, true);
+
+
+
+
+
         //affichage une confirmation
         return new Response("La pizza avec l'id  {$pizza->getId()} a bien été reregistre");
     }
+
+    #[Route('/pizza/{id}/afficher', name: 'app_pizza_show')]
+    public function show(int $id, PizzaRepository $repository): Response
+    {
+        //recuperer de la pizza qui a l'id $id
+        $pizza = $repository->find($id);
+        //affichage du nom de la pizza
+        return new Response("nom: {$pizza->getName()}");
+    }
+      //grace a frameworkExtraBundle la 2eme methode resout le id en pizza directment
+      // donc l'appel a $respository->find() est faity automatic en arriere plan
+      #[Route('/pizza/{id}/afficher2', name: 'app_pizza_show2')]
+      //symfony reconnait automatique par Pizza aulieu de 'int $id, PizzaRepository $repository' et non plus besoin de '$pizza = $repository->find($id);' 
+    public function show2(Pizza $pizza): Response
+    {
+       //affichage du nom de la pizza
+        return new Response("nom: {$pizza->getName()}");
+    }
+
 }
